007f807c11869cf87820f11299ba9a8a13aacee4
darknoon29 <darknoon@darkcity.fr>
2014-11-15 15:39:31 +0100

    Script de mise ? jour pour le passage en utf8

--- update.php
+++ update.php
@@ -12,10 +12,10 @@ global $db,$table_prefix;
 $mod_folder = "xtense";
 $mod_name = "xtense";
 
-$db->sql_query("ALTER TABLE ".TABLE_PARSEDREC." CONVERT TO CHARACTER SET utf8");
-$db->sql_query("ALTER TABLE ".TABLE_XTENSE_CALLBACKS." CONVERT TO CHARACTER SET utf8");
-$db->sql_query("ALTER TABLE ".TABLE_XTENSE_GROUPS." CONVERT TO CHARACTER SET utf8");
-$db->sql_query("ALTER TABLE ".TABLE_PARSEDSPYEN." CONVERT TO CHARACTER SET utf8");
+$db->sql_query("ALTER TABLE ".$table_prefix."parsedRec"." CONVERT TO CHARACTER SET utf8");
+$db->sql_query("ALTER TABLE ".$table_prefix."xtense_callbacks"." CONVERT TO CHARACTER SET utf8");
+$db->sql_query("ALTER TABLE ".$table_prefix."xtense_groups"." CONVERT TO CHARACTER SET utf8");
+$db->sql_query("ALTER TABLE ".$table_prefix."parsedSpyEn"." CONVERT TO CHARACTER SET utf8");
 
 
 $db->sql_query('DELETE FROM '.TABLE_CONFIG.' WHERE config_name LIKE "xtense_log_ogspy"');
