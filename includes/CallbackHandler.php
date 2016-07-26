07368ee5c64dce009062871dffe11aff177c7d16
Anthony <darknoon29@users.noreply.github.com>
2016-07-26 11:49:15 +0200

    Documentation MarkDown

---
+++ includes/CallbackHandler.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @package Xtense 2
+ * @author Unibozu
+ * @licence GNU
+ */
+
+if (!defined('IN_SPYOGAME') && !defined('IN_UNISPY2')) exit;
+
+/**
+ * Gestion des fonctions de callback des plugins OGSpyw
+ *
+ */
+class CallbackHandler {
+	private $list = array();
+	private $calls = array();
+	private $types = array();
+	private $included = array();
+	public $currentCallback = false;
+
+	/**
+	 * @param $type
+	 * @param $params
+     */
+	public function add($type, $params) {
+		if (empty($params)) return;
+		$this->calls[$type][] = $params;
+		if (!in_array($type, $this->types)) {
+			$this->types[] = $type;
+		}
+	}
+
+	/**
+	 * Appels des fonctions des mods
+	 *
+	 */
+	public function apply() {
+		global $io, $db, $get_dev, $server_config;
+		if (empty($this->calls)) return;
+		$success = array();
+		$errors = array();
+		
+		$query = $db->sql_query('SELECT c.id, c.function, c.type, c.mod_id, m.root, m.title FROM '.TABLE_XTENSE_CALLBACKS.' c LEFT JOIN '.TABLE_MOD.' m ON c.mod_id = m.id WHERE c.active = 1 AND m.active = 1 AND c.type IN ("'.implode('", "', $this->types).'")'); 
+		while ($call = $db->sql_fetch_assoc($query)) {
+			foreach ($this->calls[$call['type']] as $params) {
+				$this->currentCallback = $call;
+				
+				try {
+					$instance = Callback::load($call['root']);
+					
+					if (!method_exists($instance, $call['function']) || !is_callable(array($instance, $call['function']))) throw new Exception('Invalid method "'.$call['function'].'"');
+					
+					$execReturn = $instance->{$call['function']}($params);
+					
+					$io->append_call($call, $execReturn);
+				} catch (Mysql_Exception $e) {
+					$io->append_call_error($call, 'Erreur MySQL lors de l\'execution'."\n".$e->getFile().' @ '.$e->getLine()."\n".$e->getMessage());
+				} catch (Exception $e) {
+					$io->append_call_error($call, $e->getMessage(), $e);
+				}
+				
+				$this->currentCallback = false;
+			} // Foreach
+		} // while
+		
+	} // Method "apply"
+}
---
+++ lang/es/lang_xtense.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: antho
+ * Date: 22/03/2016
+ * Time: 15:34
+ */
+/*
+ * <?php echo($lang['ADMIN_DISPLAY_GALAXY_TITLE']); ?>
+ *
+ */
+
+/* index.php */
+
+$lang['MOD_XTENSE_LANG'] = "es";
+$lang['MOD_XTENSE_TITLE'] = "Xtenses";
+$lang['MOD_XTENSE_ADMINTITLE'] = "Administración de Xtense";
+$lang['MOD_XTENSE_INFORMATIONS'] = "Información";
+$lang['MOD_XTENSE_CONFIGURATION'] = "Configuración";
+$lang['MOD_XTENSE_PERMISSIONS'] = "Autorización";
+$lang['MOD_XTENSE_MODS'] = "Mods";
+$lang['MOD_XTENSE_ABOUT'] = "A propósito";
+$lang['MOD_XTENSE_DOWNLOAD'] = "Descargar registro";
+$lang['MOD_XTENSE_FIREFOX'] = "Firefox versión (Obtener la última versión y abrir el archivo con Firefox)";
+$lang['MOD_XTENSE_FIREFOX_LINK'] = "Modulo de Xtense para Firefox";
+$lang['MOD_XTENSE_CHROME'] = "Versión de Chrome";
+$lang['MOD_XTENSE_CHROME_LINK'] = "Módulo de Xtense en Chrome Web Store";
+$lang['MOD_XTENSE_INSTALL_HELP'] = "Documentación completa para instalar un módulo";
+$lang['MOD_XTENSE_CONNECTION_DETAILS'] = "Esta es la información que debe especificar el plugin Xtense para conectarse a este servidor";
+$lang['MOD_XTENSE_URL_UNIVERSE'] = "URL del universo";
+$lang['MOD_XTENSE_URL_PLUGIN'] = "URL del plugin de OGSpy";
+$lang['MOD_XTENSE_PSEUDO_PASSWORD'] = "Es necesario que el nick y la contraseña sea la de OGSpy";
+$lang['MOD_XTENSE_UPDATE_DONE'] = "La actualización se ha completado";
+$lang['MOD_XTENSE_REPAIR_DONE'] = "Espacio personal correctamente arreglado";
+$lang['MOD_XTENSE_CALLBACK_SUMMARY'] = "Las llamadas se han instalado.";
+$lang['MOD_XTENSE_ERROR_DETAILS'] = "Detalles de errores";
+$lang['MOD_XTENSE_CALLBACK_LIST'] = "Lista de modificaciones";
+$lang['MOD_XTENSE_INSTALLED_CALLBACKS'] = "Modificaciones instaladas correctamente";
+$lang['MOD_XTENSE_WRONG_CALLBACKS'] = "Algunos enlaces no pueden instalarse de forma automática";
+$lang['MOD_XTENSE_ALLOW_CONNECTIONS'] = "Habilitar la conexión al plugin";
+$lang['MOD_XTENSE_ALLOW_ADMIN_ONLY'] = "Límite de administración";
+$lang['MOD_XTENSE_SPYREPORTS_AUTODELETE'] = "Eliminación automática de RE demasiado viejo (configurable desde el menú de administrador de OGSpy).";
+$lang['MOD_XTENSE_SERVER_NAME'] = "Servidor de juego";
+$lang['MOD_XTENSE_LOGS'] = "Registro de accesos";
+$lang['MOD_XTENSE_SOLARSYSTEMS'] = "Sistemas solares";
+$lang['MOD_XTENSE_SPYREPORTS'] = "Reportes de espionaje";
+$lang['MOD_XTENSE_EMPIRE'] = "Imperio (Página de Imperio, Edificio, Investigaciones, ...)";
+$lang['MOD_XTENSE_RANKINGS'] = "Clasificación";
+$lang['MOD_XTENSE_ALLIANCE_LIST'] = "Lista de jugadores de la alianza";
+$lang['MOD_XTENSE_MESSAGES'] = "Mensajes";
+$lang['MOD_XTENSE_ACTIONS'] = "Acciones";
+$lang['MOD_XTENSE_REPAIR_EMPIRE'] = "Reparar los espacios personales (en caso de problemas con el espacio personal completo)";
+$lang['MOD_XTENSE_INSTALL_CALLBACKS'] = "Installer les appels de tous les mods installés et activés";
+$lang['MOD_XTENSE_SEND'] = "Enviar";
+$lang['MOD_XTENSE_CANCEL'] = "Anular";
+$lang['MOD_XTENSE_GROUPS_DEFINITION'] = "Se puede establecer para cada OGSpy el acceso que los usuarios Xtense.";
+$lang['MOD_XTENSE_UPDATE_DONE'] = "La actualización se ha completado";
+$lang['MOD_XTENSE_TICKALL'] = "Marcar todas";
+$lang['MOD_XTENSE_UNTICKALL'] = "Desmarcar todas";
+$lang['MOD_XTENSE_CALLBACK_LIST_DESC'] = "Lista de modificaciones relacionadas Xtense plugin. Estos enlaces permiten mods para recuperar los datos enviados por Xtense. Aquí puede activar o desactivar estos enlaces.";
+$lang['MOD_XTENSE_CALLBACK_MODNAME'] = "Número de versión del mod";
+$lang['MOD_XTENSE_CALLBACK_DATATYPE'] = "Tipo de datos";
+$lang['MOD_XTENSE_CALLBACK_STATUSMOD'] = "Estado del mod";
+$lang['MOD_XTENSE_CALLBACK_STATUSLINK'] = "Estado del enlace";
+$lang['MOD_XTENSE_CALLBACK_ENABLED'] = "Enlace activo";
+$lang['MOD_XTENSE_CALLBACK_DISABLED'] = "Enlace inactivo";
+$lang['MOD_XTENSE_MOD_ENABLED'] = "Módulo activo";
+$lang['MOD_XTENSE_MOD_DISABLED'] = "Módulo inactivo";
+$lang['MOD_XTENSE_CALLBACK_NONE'] = "Ningún registro en la base de datos";
+$lang['MOD_XTENSE_AUTHOR'] = "Xtense para Unibozu";
+$lang['MOD_XTENSE_FORUM'] = "Foro de soporte de OGSteam";
+$lang['MOD_XTENSE_ICONS'] = "Set de iconos";
+$lang['MOD_XTENSE_CHANGELOG'] = "Changelog";
+$lang['MOD_XTENSE_CHANGELOG_LINK'] = "Enlace para visualizar changelog";
+$lang['MOD_XTENSE_SUPPPORT'] = "Soporte";
