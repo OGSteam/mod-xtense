ea9a0d65306caa4621eb90188b32534c19f54b7d
antchoma <antchoma@bouyguestelecom.fr>
2017-01-12 19:07:01 +0100

    Rework scripts installation et paramametres

--- install.php
+++ install.php
@@ -5,16 +5,13 @@
  * @licence GNU
  */
 
-if (!defined('IN_SPYOGAME')) die("Hacking Attemp!");
+if (!defined('IN_SPYOGAME')) die("Hacking Attempt!");
 
 $root = $pub_directory;
 $install_ogspy = false;
-$is_ok = false;
-$mod_folder = "xtense";
-$is_ok = install_mod ($mod_folder);
+
 $db->sql_query("UPDATE ".TABLE_MOD." SET menu = '<span onclick=\"window.open(this.parentNode.href, \'Xtense\', \'width=750, height=550, menubar=no, resizable=yes, scrollbars=yes, status=no, toolbar=no\'); return false;\">Xtense</span>' WHERE title = 'xtense'");
-if ($is_ok == true)
-{
+
 	if ( file_exists (str_replace('install', '', getcwd()).'mod/'.$root.'/includes/config.php'))
 	{
 		require_once(str_replace('install', '', getcwd()).'mod/'.$root.'/includes/config.php');
@@ -25,7 +22,7 @@ if ($is_ok == true)
 		require_once('mod/'.$root.'/includes/config.php');
 
 		//---- Creation de la table des recyclages
-		$db->sql_query("CREATE TABLE IF NOT EXISTS ".TABLE_PARSEDREC." (
+        mod_create_table("CREATE TABLE IF NOT EXISTS ".TABLE_PARSEDREC." (
 			`id_rec` INT( 255 ) NOT NULL AUTO_INCREMENT ,
 			`dateRec` INT( 11 ) NOT NULL ,
 			`coordinates` VARCHAR( 9 ) NOT NULL ,
@@ -37,8 +34,8 @@ if ($is_ok == true)
 			`sender_id` INT( 11 ) NOT NULL ,
 			PRIMARY KEY ( `id_rec` )
 		) DEFAULT CHARSET=utf8;");
-        
-        $db->sql_query("CREATE TABLE IF NOT EXISTS ".TABLE_PARSEDSPYEN." (
+
+        mod_create_table("CREATE TABLE IF NOT EXISTS ".TABLE_PARSEDSPYEN." (
             `spy_id` INT( 255 ) NOT NULL AUTO_INCREMENT ,
             `dateSpy` INT( 11 ) NOT NULL ,
             `from` VARCHAR( 9 ) NOT NULL ,
@@ -49,7 +46,7 @@ if ($is_ok == true)
         )DEFAULT CHARSET=utf8;");
 
 		//---- Creation de la table des Callbacks
-		$db->sql_query("CREATE TABLE IF NOT EXISTS `".TABLE_XTENSE_CALLBACKS."` (
+        mod_create_table("CREATE TABLE IF NOT EXISTS `".TABLE_XTENSE_CALLBACKS."` (
 			`id` int(3) NOT NULL auto_increment,
 			`mod_id` int(3) NOT NULL,
 			`function` varchar(30) NOT NULL,
@@ -60,7 +57,7 @@ if ($is_ok == true)
 			KEY `active` (`active`)
 			) DEFAULT CHARSET=utf8;");
 
-		$db->sql_query("CREATE TABLE IF NOT EXISTS `".TABLE_XTENSE_GROUPS."` (
+        mod_create_table("CREATE TABLE IF NOT EXISTS `".TABLE_XTENSE_GROUPS."` (
 			`group_id` int(4) NOT NULL,
 			`system` tinyint(4) NOT NULL,
 			`ranking` tinyint(4) NOT NULL,
@@ -70,19 +67,19 @@ if ($is_ok == true)
 			) DEFAULT CHARSET=utf8;");
 
 		//---- Creation configuration Xtense
-		$db->sql_query("REPLACE INTO ".TABLE_CONFIG." (config_name, config_value) VALUES
-			('xtense_allow_connections', '1'),
-			('xtense_log_empire', '0'),
-			('xtense_log_ranking', '1'),
-			('xtense_log_spy', '1'),
-			('xtense_log_system', '1'),
-			('xtense_log_ally_list', '1'),
-			('xtense_log_messages', '1'),
-			('xtense_log_reverse', '0'),
-			('xtense_strict_admin', '0'),
-			('xtense_universe', 'http://sxx-fr.ogame.gameforge.com'),
-			('xtense_spy_autodelete', '1')
-		");
+
+        mod_set_option('xtense_allow_connections', '1');
+        mod_set_option('xtense_log_empire', '0');
+        mod_set_option('xtense_log_ranking', '1');
+        mod_set_option('xtense_log_spy', '1');
+        mod_set_option('xtense_log_system', '1');
+        mod_set_option('xtense_log_ally_list', '1');
+        mod_set_option('xtense_log_messages', '1');
+        mod_set_option('xtense_log_reverse', '0');
+        mod_set_option('xtense_strict_admin', '0');
+        mod_set_option('xtense_universe', 'https://sxx-fr.ogame.gameforge.com');
+        mod_set_option('xtense_spy_autodelete', '1');
+
 		generate_config_cache();
 		$db->sql_query("REPLACE INTO ".TABLE_XTENSE_GROUPS." (`group_id`, `system`, `ranking`, `empire`, `messages`) VALUES
 			('1', '1', '1', '1', '1')");
@@ -95,5 +92,5 @@ if ($is_ok == true)
 		{		
 			echo "<a onclick=\"window.open('index.php?action=Xtense', 'Xtense', 'width=720, height=500, menubar=no, resizable=yes, scrollbars=yes, status=no, toolbar=no');\"><font color='red' size='9'>Autorisez la pop-up ou cliquez ici!</font></a><script type=\"text/javascript\"><!-- \nwindow.onload = window.open('index.php?action=Xtense', 'Xtense', 'width=720, height=500, menubar=no, resizable=yes, scrollbars=yes, status=no, toolbar=no');\n--></script>";
 		}
-	}
+
 }
