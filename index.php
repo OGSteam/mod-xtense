fd7656f1c15e6ffa152f8b5cedd351e63d3904b7
darknoon29 <darknoon@darkcity.fr>
2017-01-30 00:24:35 +0100

    A ne pas faire car la BDD est utilisée par la suite.

--- index.php
+++ index.php
@@ -129,7 +129,7 @@ if ($page == 'mods') {
 }
 $php_end = benchmark();
 $php_timing = $php_end - $php_start;
-$db->sql_close();
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
     "http://www.w3.org/TR/2002/REC-xhtml1-20020801/DTD/xhtml1-transitional.dtd">
