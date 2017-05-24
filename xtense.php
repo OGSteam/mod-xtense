5276bb6f46c5e37afc509333f69f4e30f8fe174b
Pascal COLIN <pascalcolin3@yahoo.fr>
2017-05-24 22:40:17 +0200

    ligne de test

--- xtense.php
+++ xtense.php
@@ -746,7 +746,6 @@ switch ($page_type) {
 
     case 'rc': //PAGE RC
     case 'rc_shared':
-        $pub_json = file_get_contents(@'D:\Projets\Ogsteam\rc4.json');
         if (isset($pub_json) == false) die("hack");
         if(!isset($pub_ogapilnk))
             $pub_ogapilnk = '';
